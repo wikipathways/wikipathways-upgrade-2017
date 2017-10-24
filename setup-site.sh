f8c6aee0dff2a006a2759ba1d7565bd220f120ae
Mark A. Hershberger <mah@nichework.com>
2017-10-24 08:51:20 -0700

    Script to deploy after checkout

---
+++ setup-site.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -e
+
+git submodule init
+git submodule update
+for i in composer.lock vendor composer.local.json LocalSettings.php; do
+    rm mediawiki/$i
+    ln -s ../$i mediawiki
+done
+
+for i in extensions/* skins/*; do
+    rm mediawiki/$i
+    ln -s ../../$i mediawiki/$i
+done
+
+echo installing mbstring, mysql, and xml for php
+sudo apt install php-mbstring php-mysql php-xml
