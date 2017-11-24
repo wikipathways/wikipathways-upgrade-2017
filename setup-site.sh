067b07410413293a2b4a89477b1c1edf3b299ec8
Mark A. Hershberger <mah@nichework.com>
2017-11-23 21:53:00 -0500

    Clean up various issues
    * Address nagging bits of phpcs in LocalSettings.php
    * Update README with helpful details
    * Add dependency in composer for symfony/process
    * Add dependency in setup-site.sh on python-pygments

--- setup-site.sh
+++ setup-site.sh
@@ -3,17 +3,16 @@
 git submodule init
 git submodule update
 for i in composer.lock vendor composer.local.json LocalSettings.php; do
-    rm mediawiki/$i
-    ln -s ../$i mediawiki
+	rm mediawiki/$i
+	ln -s ../$i mediawiki
 done
 
 for i in extensions/* skins/*; do
-    rm mediawiki/$i
-    ln -s ../../$i mediawiki/$i
+	rm mediawiki/$i
+	ln -s ../../$i mediawiki/$i
 done
 
 echo installing mbstring, mysql, and xml for php
-sudo apt install php-mbstring php-mysql php-xml
+sudo apt install php-mbstring php-mysql php-xml python-pygments
 
 chmod 1777 mediawiki/images
-
