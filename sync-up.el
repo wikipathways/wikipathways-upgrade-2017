82d58801111a67e12856edd28829bc2a14be9f90
Mark A. Hershberger <mah@nichework.com>
2017-11-23 22:55:47 -0500

    Document and make an essential bit of my workflow documented.

---
+++ sync-up.el
@@ -0,0 +1,22 @@
+;;; sync-up --- Set up synchronization with vm1
+
+;;; Commentary:
+;; A file to enable setup for synchronization upon save in my local
+;; Emacs.  TRAMP could be used, but for working on projects, working
+;; on a local copy is really better.
+
+;;; Code:
+
+(when (not (fboundp 'package-installed-p))
+  (package-initialize))
+(when (not (package-installed-p 'auto-shell-command))
+  (package-install 'auto-shell-command))
+
+(ascmd:add
+ (list "/home/mah/client/gladstone/new.wikipathways.org/"
+	   (concat "rsync -a --delete --exclude .git --exclude mediawiki/images "
+			   "/home/mah/client/gladstone/new.wikipathways.org/ "
+			   "vm1:/home/wikipathways.org/")))
+
+(provide 'sync-up)
+;;; sync-up ends here
