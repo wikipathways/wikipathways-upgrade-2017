7502f2547406392a4c97848e8aba438a773ad390
Mark A. Hershberger <mah@nichework.com>
2018-01-27 20:01:28 -0500

    Clean up some linter warnings

--- extensions/WikiPathways/Pager/CliPathwaysPager.php
+++ extensions/WikiPathways/Pager/CliPathwaysPager.php
@@ -6,7 +6,11 @@
 
 class CliPathwaysPager extends BasePathwaysPager {
 
-	function nextPager( $offset ) {
+	/**
+	 * @param int $offset to start at
+	 * @return CliPathwaysPager
+	 */
+	public function nextPager( $offset ) {
 		self::$myOffset = $offset;
 		self::$myLimit = 50;
 		self::$myBackwards = false;
@@ -15,7 +19,10 @@ class CliPathwaysPager extends BasePathwaysPager {
 		return new self();
 	}
 
-	static function initPager() {
+	/**
+	 * @return CliPathwaysPager
+	 */
+	public static function initPager() {
 		self::$myOffset = null;
 		self::$myLimit = 50;
 		self::$myBackwards = false;
@@ -25,31 +32,57 @@ class CliPathwaysPager extends BasePathwaysPager {
 	}
 
 	// set these directly for now.
-	static $myOffset;
-	static $myLimit;
-	static $myBackwards;
-	static $myOrder;
+	protected static $myOffset;
+	protected static $myLimit;
+	protected static $myBackwards;
+	protected static $myOrder;
 
+	/**
+	 * @return int
+	 */
 	public function getOffset() {
 		return self::$myOffset;
 	}
+	/**
+	 * @return int
+	 */
 	public function getLimit() {
 		return self::$myLimit;
 	}
+
+	/**
+	 * @return bool
+	 */
 	public function isBackwards() {
 		return self::$myBackwards;
 	}
+	/**
+	 * @return string
+	 */
 	public function getOrder() {
 		return self::$myOrder;
 	}
+
+	/**
+	 * @param int $row bogus
+	 * @throws MWException
+	 */
 	public function formatRow( $row ) {
 		throw new MWException( "You shouldn't see this!" );
 	}
 
+	/**
+	 * @param DatabaseResult $res to use
+	 * @return string
+	 */
 	function getKey( $res ) {
 		return $res->page_title;
 	}
 
+	/**
+	 * @param DatabaseResult $res to use
+	 * @return string
+	 */
 	function getValue( $res ) {
 		return $res->tag_text;
 	}
