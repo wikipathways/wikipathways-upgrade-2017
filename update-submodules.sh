d36e1be07f58497be170650234e6014299afdb58
Anders Riutta <git@andersriutta.com>
2018-10-18 16:11:09 -0700

    Changes needed to make update-submodules.sh work

--- update-submodules.sh
+++ update-submodules.sh
@@ -23,35 +23,32 @@ if [ -d "$mediawiki_dir" ] && [ -d "$mediawiki_dir/.git" ]; then
 fi
 
 
-### TODO: should we sync?
-###git submodule sync && git submodule update --init --recursive
-#git submodule update --init --recursive
-##git submodule update --init
-#
-#for i in composer.lock vendor composer.local.json LocalSettings.php package-lock.json; do
-#	rm -rf mediawiki/$i
-#	ln -s ../$i mediawiki
-#done
-#
-#for i in extensions/* skins/*; do
-#    if [ ! -d mediawiki/$i ]; then
-#        rm -rf mediawiki/$i
-#	ln -s ../$i mediawiki/$i
-#    fi
-#done
-#
-#rm -f mediawiki/.htaccess
-#ln -s ../htaccess mediawiki/.htaccess
-#
-#rm -f mediawiki/images/wikipathways
-#ln -s ../images/wikipathways mediawiki/images/wikipathways
-#
-#rm -f mediawiki/images/wpi
-#ln -s ../images/wpi mediawiki/images/wpi
-#
-#dir=mediawiki/images
-#stdir=`stat -c %a mediawiki/images`
-#if [ $stdir -ne 1777 ]; then
-#	#echo 'making images writable...'
-#	sudo chmod 1777 $dir
-#fi
+git submodule sync && git submodule update --init --recursive
+
+for i in composer.lock vendor composer.local.json LocalSettings.php package-lock.json; do
+	rm -rf mediawiki/$i
+	ln -s ../$i mediawiki
+done
+
+for i in extensions/* skins/*; do
+    if [ ! -d mediawiki/$i ]; then
+        rm -rf mediawiki/$i
+	ln -s ../$i mediawiki/$i
+    fi
+done
+
+rm -f mediawiki/.htaccess
+ln -s ../htaccess mediawiki/.htaccess
+
+rm -f mediawiki/images/wikipathways
+ln -s ../images/wikipathways mediawiki/images/wikipathways
+
+rm -f mediawiki/images/wpi
+ln -s ../images/wpi mediawiki/images/wpi
+
+dir=mediawiki/images
+stdir=`stat -c %a mediawiki/images`
+if [ $stdir -ne 1777 ]; then
+	#echo 'making images writable...'
+	sudo chmod 1777 $dir
+fi
