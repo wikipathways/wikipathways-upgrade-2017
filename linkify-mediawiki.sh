d3b10befafa2fddf788a98b14dfe36cdac7a59ee
Anders Riutta <git@andersriutta.com>
2018-10-24 18:25:35 -0700

    Pull in chages from dev

---
+++ linkify-mediawiki.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -e
+
+mediawiki_dir="mediawiki"
+
+for i in composer.lock vendor composer.local.json LocalSettings.php package-lock.json; do
+	rm -rf "$mediawiki_dir/$i"
+	ln -s ../$i "$mediawiki_dir"
+done
+
+for i in extensions/* skins/*; do
+    if [ ! -d "$mediawiki_dir/$i" ]; then
+        rm -rf "$mediawiki_dir/$i"
+	ln -s ../../$i "$mediawiki_dir/$i"
+    fi
+done
+
+rm -f "$mediawiki_dir"/.htaccess
+ln -s ../htaccess "$mediawiki_dir/.htaccess"
+
+rm -rf "$mediawiki_dir/images"
+ln -s ../images "$mediawiki_dir/images"
--- update-submodules.sh
+++ update-submodules.sh
@@ -2,7 +2,7 @@
 
 mediawiki_dir="mediawiki"
 
-bash ./revert-mediawiki.sh
+. ./revert-mediawiki.sh
 
 git submodule sync && git submodule update --init --recursive
 
@@ -11,27 +11,4 @@ cd "$mediawiki_dir"
 composer update
 cd ..
 
-for i in composer.lock vendor composer.local.json LocalSettings.php package-lock.json; do
-	rm -rf mediawiki/$i
-	ln -s ../$i mediawiki
-done
-
-for i in extensions/* skins/*; do
-    if [ ! -d mediawiki/$i ]; then
-        rm -rf mediawiki/$i
-	ln -s ../../$i mediawiki/$i
-    fi
-done
-
-rm -f mediawiki/.htaccess
-ln -s ../htaccess mediawiki/.htaccess
-
-rm -rf mediawiki/images
-ln -s ../images mediawiki/images
-
-dir=./images
-stdir=`stat -c %a $dir`
-if [ $stdir -ne 1777 ]; then
-	#echo 'making images writable...'
-	sudo chmod 1777 $dir
-fi
+. ./linkify-mediawiki.sh
