eea7a9c000d11e6c7c8078543512e233e6cc980d
Mark A. Hershberger <mah@nichework.com>
2018-11-24 15:55:26 -0500

    Handle images better

--- setup/delinkify-mediawiki.sh
+++ setup/delinkify-mediawiki.sh
@@ -4,10 +4,13 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 . $DIR/link-files
 
+test -e "$MW_INSTALL_PATH/.htaccess" && rm -f "$MW_INSTALL_PATH/.htaccess" || true
+
+# Handling images seperately because it may be a dir in the original checkout
+test -e "$MW_INSTALL_PATH/images" && rm -rf "$MW_INSTALL_PATH/images" || true
+
 # We use '|| true' here because we want to continue even if these files don't exist
 for i in $topdirFile $subdirFile; do
 	test -L "$MW_INSTALL_PATH/$i" && rm "$MW_INSTALL_PATH/$i" || true
 done
-
-test -e "$MW_INSTALL_PATH/.htaccess" && rm -f "$MW_INSTALL_PATH/.htaccess" || true
 
--- setup/linkify-mediawiki.sh
+++ setup/linkify-mediawiki.sh
@@ -5,18 +5,18 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 . $DIR/link-files
 
 for i in $topdirFile; do
-    test -e "$MW_INSTALL_PATH/$i" || ln -s ../$i "$MW_INSTALL_PATH/$i"
+	test -e "$MW_INSTALL_PATH/$i" || ln -s ../$i "$MW_INSTALL_PATH/$i"
 done
 
 for i in $subdirFile; do
-    if [ -d "$MW_INSTALL_PATH/$i" -a ! -L "$MW_INSTALL_PATH/$i" ]; then
-        echo $i is in core MW now.  You should probably remove it from extensions or fix this script.
-        exit 1
-    else
-        if [ ! -L  "$MW_INSTALL_PATH/$i" ]; then
-            ln -s ../../$i "$MW_INSTALL_PATH/$i"
-        fi
-    fi
+	if [ -d "$MW_INSTALL_PATH/$i" -a ! -L "$MW_INSTALL_PATH/$i" ]; then
+		echo $i is in core MW now.  You should probably remove it from extensions or fix this script.
+		exit 1
+	else
+		if [ ! -L  "$MW_INSTALL_PATH/$i" ]; then
+			ln -s ../../$i "$MW_INSTALL_PATH/$i"
+		fi
+	fi
 done
 
 test -e "$MW_INSTALL_PATH/.htaccess" || ln -s ../htaccess "$MW_INSTALL_PATH/.htaccess"
